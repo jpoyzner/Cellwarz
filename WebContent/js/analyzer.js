5e865aa161ee3a1d90371f7e2c83901e0b7ea8d7
Jeff Poyzner <holidaydoctor@hotmail.com>
2014-05-27 03:44:17 -0700

    analyzer added

---
+++ WebContent/js/analyzer.js
@@ -0,0 +1,20 @@
+define(['backbone', 'underscore'], function() {
+	return Backbone.View.extend({
+		//TODO: current issue:
+		//Entire cell's worth of sprite updates are sent to client, where it takes too long to draw all of them
+		//the offset thing is only showing ones new offset, but might still be drawing all of them
+		
+		initialize: function(options) {
+			this.state = "-BEGIN ANALYSIS-";
+			this.connections = 0;
+			this.redraws = 0;
+			setInterval(_.bind(this.recordState, this), 1000);
+		},
+		recordState: function() {
+			this.state = this.connections + "CONNEX, " + this.redraws + " REDRAWS, REDRAW TIME:" + this.drawTime;
+			
+			this.connections = 0;
+			this.redraws = 0;
+		}
+	});
+});
