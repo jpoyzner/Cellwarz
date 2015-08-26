5059ee7cf650ac5a37cef3e8b5ff5ba4a1a701ec
Jeff Poyzner <holidaydoctor@hotmail.com>
2015-08-26 02:40:36 -0700

    project can now be build, deployed, and debugged with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,40 @@
 var gulp = require('gulp');
-var shell = require('gulp-shell');
 
-gulp.task('deploy', shell.task([
-   "scp -i ../PoyznerKey.pem ../Cellwarz.war ec2-user@52.8.176.59:Cellwarz.war"
- ]));
-
-gulp.task('default', ['deploy'], function() {
-    console.log("Finished Cellwarz tasks!");
+//TODO: REPLACE JS IN WEBAPP WITH JS DEPLOYED TO S3!!! Then make one gulp task to deploy all JS!!!
+gulp.task('default', ['bundle'], function() {
+    console.log("deplying Cellwarz JavaScript!");
 });
+
+gulp.task('bundle', ['clean'], function() {
+	var bundle =
+		require('gulp-requirejs')({
+	        baseUrl: 'js',
+	        name: 'cellwarz',
+	        out: 'cellwarz.js',
+	        paths: {
+	            jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min',
+	            jquerymobile: '//cdnjs.cloudflare.com/ajax/libs/jquery-mobile/1.4.1/jquery.mobile.min',
+	            underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
+	            backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min'
+	        }
+	    });
+    
+    bundle.pipe(require('gulp-uglify')()).pipe(gulp.dest('./WebContent/js/'));
+});
+
+gulp.task('debug', ['clean'], function() {
+	gulp.src('./js/**/*').pipe(gulp.dest('./WebContent/js/'));
+});
+
+gulp.task('clean', function(callback) {
+	require('del')(["WebContent/js/**/*"], callback);
+});
+
+
+//Unfortunately, gulp-war does not work for websockets when creating war files,
+//probably because when eclipse creates war from web project, it optimizes it using
+//a specific runtime environment?
+//ACTUALLY, IT'S POSSIBLE THE WAR FILE WOULD HAVE WORKED IF U RESTART TOMCAT?
+gulp.task('deploy', require('gulp-shell').task([
+ "scp -i ../PoyznerKey.pem ../Cellwarz.war ec2-user@52.8.176.59:Cellwarz.war"
+]));
--- js/cellwarz.js
+++ js/cellwarz.js
@@ -1,5 +1,5 @@
 requirejs.config({
-    paths: {
+    paths: { //Be sure to add these in gulpfile.js also:
         jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min',
         jquerymobile: '//cdnjs.cloudflare.com/ajax/libs/jquery-mobile/1.4.1/jquery.mobile.min',
         underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
