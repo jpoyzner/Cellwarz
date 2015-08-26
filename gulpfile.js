d2f5310ff4e70113a91dba59da09cd5e6cb67396
Jeff Poyzner <holidaydoctor@hotmail.com>
2015-08-26 00:02:26 -0700

    added gulp deployment

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,7 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
-//var war = require('gulp-war');
-//var zip = require('gulp-zip');
-
-//gulp.task('war', function () {
-//    gulp.src(["WebContent/**"])
-//    	.pipe(war({welcome: 'cell.html', displayName: 'Cellwarz WAR'}))
-//    	.pipe(zip('Cellwarz.war'))
-//        .pipe(gulp.dest(".."));
-//});
 
 gulp.task('deploy', shell.task([
-   //"sh deploy.sh"
    "scp -i ../PoyznerKey.pem ../Cellwarz.war ec2-user@52.8.176.59:Cellwarz.war"
  ]));
 
